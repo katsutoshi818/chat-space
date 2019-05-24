d5eefc52bdaf3b7822e5a82463986a3c6c6c01b1
katsutoshi818 <katsutoshi8988@gmail.com>
2019-05-24 15:20:20 +0900

    Updata.message.js_add_html

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,16 @@
 $(function(){
+  function buildHTML(comment){
+    var html = `%div.message-box
+                  %p.message-box__user-name  
+                    = ${message.user_name}
+                    %span 
+                      = ${message.create_time}.strftime("%Y/%m/%d %H:%M")
+                  - if message.content.present?
+                    %p.message-box__message 
+                      = ${message.content}
+                  =image_tag ${message.image}, class: "" if message.image.present?`
+    return html;
+  }
   $('.form-space').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +22,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-    });
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('..main-chat-space').append(html)
+      $('.textbox').val('form-space-message-area__message')
+    })
   })
-})
+});
