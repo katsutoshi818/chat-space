06c9acf5ddb02c1f9c795224dde1ff06b8dca0f7
katsutoshi818 <katsutoshi8988@gmail.com>
2019-05-24 14:40:52 +0900

    Updata_message.js_add_ivent_ajax

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,3 +1,15 @@
 $(function(){
-  
+  $('.form-space').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "post",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
 })
