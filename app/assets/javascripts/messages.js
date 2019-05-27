f98f93d7db7f2ca89ef7a005bb094b29f6aaa783
katsutoshi818 <katsutoshi8988@gmail.com>
2019-05-27 19:16:00 +0900

    add_var_reloadMessages

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -3,7 +3,7 @@ $(function(){
     var img = message.image 
       ? ` <img src=${message.image} > `
       : "";
-    var html = `<div class="message-box">
+    var html = `<div class="message-box" data-id="${message.id}">
                   <p class="message-box__user-name"> 
                     ${message.user_name}
                     <span>
@@ -43,4 +43,32 @@ $(function(){
       alert('error');
     })
   })
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = message.data('id')
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: '/api/messages',
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      messages.forEach(function (message) {
+        insertHTML = insertHTML + buildHTML(message);
+      })
+      //メッセージが入ったHTMLを取得
+      var message_list = $('.main-chat-space');
+      //メッセージを追加
+      message_list.append(insertHTML)
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 });
