ec169e51fc94b18bbf7258eeb3c89719e6dc0699
katsutoshi818 <katsutoshi8988@gmail.com>
2019-05-31 17:28:35 +0900

    for_capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,19 +7,19 @@ set :repo_url,  "git@github.com:katsutoshi818/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids" "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads")
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, "2.5.1" #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
-set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/hayakatsu.pem']  
+set :ssh_options, auth_methods: ["publickey"],
+                  keys: ["~/.ssh/hayakatsu.pem"]  
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-after 'deploy:publishing', 'deploy:restart'
+after "deploy:publishing", "deploy:restart"
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke "unicorn:restart"
   end
 end
 # lock "~> 3.11.0"
